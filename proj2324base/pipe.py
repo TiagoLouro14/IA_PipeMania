c7e6d50a1e6f95a5e1908b62100123650ff429dd
Tiago Louro <tiagolouro22@hotmail.com>
2024-05-09 22:31:44 +0100

    rodar 90 graus clockwise e nao, tem tbm o teste do enunciado mas ainda nao aparece a parte do resultado

--- proj2324base/pipe.py
+++ proj2324base/pipe.py
@@ -8,6 +8,7 @@
 
 from sys import stdin
 import numpy as np
+from copy import deepcopy
 from search import Problem
 from search import (
     Problem,
@@ -39,7 +40,8 @@ class Board:
 
     def __init__(self, board_list, size:int):
         self.board = np.array(board_list)
-        self.pieces = {"FC","FB","FE","FD","BC","BB","BE","BD","VC","VB","VE","VD","LH","LV"}
+        self.pieces = {"FC","FB","FE","FD","BC","BB",
+                       "BE","BD","VC","VB","VE","VD","LH","LV"}
         self.columns = self.rows = size
           
               
@@ -69,9 +71,11 @@ class Board:
         # Split each line into pieces by two spaces using list comprehension
         board_list = [line.split('  ') for line in lines if line]
 
-        # Determine the size of the board
-        size = len(board_list)
-        return Board(board_list, size)
+        return Board(board_list, len(board_list))
+    
+    def __str__(self):
+        """Return a string representation of the board."""
+        return '\n'.join('  '.join(row) for row in self.board)
     
     # TODO: outros metodos da classe
 
@@ -83,31 +87,103 @@ class PipeMania(Problem):
         pass
 
     def actions(self, state: PipeManiaState):
-        """Retorna uma lista de ações que podem ser executadas a
-        partir do estado passado como argumento."""
-        # TODO
-        pass
-
+        """Return a list of actions that can be executed from the given state."""
+        actions = []
+        for row in range(state.board.shape[0]):
+            for col in range(state.board.shape[1]):
+                # Add actions to rotate the piece at (row, col) in both directions
+                actions.append((row, col, True))
+                actions.append((row, col, False))
+        return actions
+    
+    def rotate_piece(self,piece, clockwise):
+        """Rotate the given piece in the specified direction."""
+        if(piece == "FC"):
+            return "FD" if clockwise else "FE"
+        
+        if(piece == "FD"):
+            return "FB" if clockwise else "FC"
+        
+        if(piece == "FB"):
+            return "FE" if clockwise else "FD"
+        
+        if(piece == "FE"):
+            return "FC" if clockwise else "FB"       
+        
+        if(piece == "BC"):
+            return "BD" if clockwise else "BE"
+        
+        if(piece == "BD"):
+            return "BB" if clockwise else "BC" 
+        
+        if(piece == "BB"):
+            return "BE" if clockwise else "BD"
+        
+        if(piece == "BE"):
+            return "BC" if clockwise else "BB"
+        
+        if(piece == "VC"):
+            return "VD" if clockwise else "VE"
+        
+        if(piece == "VD"):
+            return "VB" if clockwise else "VC"
+        
+        if(piece == "VB"):
+            return "VE" if clockwise else "VD"
+        
+        if(piece == "VE"):
+            return "VC" if clockwise else "VB"
+        
+        if(piece == "LH"):
+            return "LV" if clockwise else "LH"
+        
+        if(piece == "LV"):
+            return "LH" if clockwise else "LV"
+        
+        
     def result(self, state: PipeManiaState, action):
-        """Retorna o estado resultante de executar a 'action' sobre
-        'state' passado como argumento. A ação a executar deve ser uma
-        das presentes na lista obtida pela execução de
-        self.actions(state)."""
-        # TODO
-        pass
+        """Return the state resulting from applying the given action to the given state."""
+        # Create a copy of the state
+        new_state = deepcopy(state)
 
-    def goal_test(self, state: PipeManiaState):
+        # Unpack the action
+        row, col, clockwise = action
+
+        # Rotate the piece at (row, col) in the specified direction
+        new_state.board.board[row][col] = self.rotate_piece(new_state.board.board[row][col], clockwise)
+
+        return new_state
+
+    def goal_test(self, piece, clockwise):
         """Retorna True se e só se o estado passado como argumento é
         um estado objetivo. Deve verificar se todas as posições do tabuleiro
         estão preenchidas de acordo com as regras do problema."""
         # TODO
         pass
+    
+    def needs_rotation(self, piece):
+        """Return True if the given piece needs to be rotated to match the goal."""
+        if(piece == "FC" or piece == "BC" or piece == "VC"):
+            return True
+        return False
 
     def h(self, node: Node):
-        """Função heuristica utilizada para a procura A*."""
-        # TODO
-        pass
-    
+        """Heuristic function used for A* search."""
+        # Get the current state
+        state = node.state
+
+        # Initialize count of pieces that need to be rotated
+        count = 0
+
+        # Iterate over each piece in the board
+        for row in range(state.board.shape[0]):
+            for col in range(state.board.shape[1]):
+                # If the piece needs to be rotated, increment the count
+                if self.needs_rotation(state.board[row][col]):
+                    count += 1
+
+        return count
+
     def __str__(self):
         return '\n'.join(' '.join(row) for row in self.board_list)
     # TODO: outros metodos da classe
@@ -132,8 +208,23 @@ if __name__ == "__main__":
 
 # Ler grelha do figura 1a:
 board = Board.parse_instance()
+print(board)
 
-#print(board.adjacent_vertical_values(0, 0))
-#print(board.adjacent_horizontal_values(0, 0))
-#print(board.adjacent_vertical_values(1, 1))
-#print(board.adjacent_horizontal_values(1, 1))
+problem = PipeMania(board)
+# Criar um estado com a configuração inicial:
+s0 = PipeManiaState(board)
+# Aplicar as ações que resolvem a instância
+s1 = problem.result(s0, (0, 1, True))
+s2 = problem.result(s1, (0, 1, True))
+s3 = problem.result(s2, (0, 2, True))
+s4 = problem.result(s3, (0, 2, True))
+s5 = problem.result(s4, (1, 0, True))
+s6 = problem.result(s5, (1, 1, True))
+s7 = problem.result(s6, (2, 0, False)) # anti-clockwise (exemplo de uso) 
+s8 = problem.result(s7, (2, 0, False)) # anti-clockwise (exemplo de uso) 
+s9 = problem.result(s8, (2, 1, True))
+s10 = problem.result(s9, (2, 1, True))
+s11 = problem.result(s10, (2, 2, True))
+
+print("\nsolution?:\n")
+print(s11.board)
